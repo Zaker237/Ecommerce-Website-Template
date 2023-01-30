d05368f7a7dda51463f54b9120a43e67c1eb9704
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-01-30 03:07:52 +0100

    define header store and transform toast to store

--- src/app.js
+++ src/app.js
@@ -1,45 +1,52 @@
 document.addEventListener('alpine:init', () => {
-	Alpine.data('index', () => ({
+	Alpine.store('header', {
 		cartItems: 0,
 		watchingItems: [],
-		get watchlistItems(){
+		get watchlistItems() {
 			return this.watchingItems.length;
 		},
-		addToCart() {
-			this.cartItems++;
-			this.toast.show('The item was added into the cart');
-		},
-		addToWatchlist(id) {
-			if(this.watchingItems.includes(id)){
-				this.watchingItems.splice(this.watchingItems.indexOf(id), 1);
-				this.toast.show('The item was removed into the watchlist');
-			}else{
-				this.watchingItems.push(id);
-				this.toast.show('The item was added into the watchlist');
-			}
-		},
-		isInWatchlist(id){
-			return this.watchingItems.includes(id);
-		},
-		toast: {
-			visible: false,
-			delay: 5000,
-			percent: 0,
-			interval: null,
-			timeout: null,
-			message: null,
-			close(){
-				this.visible = false;
-				clearInterval(this.interval);
-			},
-			show(message){
-				this.visible = true;
-				this.message = message;
+	});
 
-				if(this.interval){
+	Alpine.store('toast', {
+		visible: false,
+		delay: 5000,
+		percent: 0,
+		interval: null,
+		timeout: null,
+		message: null,
+		close() {
+			this.visible = false;
+			clearInterval(this.interval);
+		},
+		show(message) {
+			this.visible = true;
+			this.message = message;
+
+			if (this.interval) {
+				clearInterval(this.interval);
+				this.interval = null;
+			}
+
+			if (this.timeout) {
+				clearTimeout(this.timeout);
+			}
+
+			this.timeout = setTimeout(() => {
+				this.visible = false;
+				this.timeout = null;
+			}, this.delay);
+			const startDate = Date.now();
+			const futureDate = Date.now() + this.delay;
+			this.interval = setInterval(() => {
+				const date = Date.now();
+				this.percent = (date - startDate) * 100 / (futureDate - startDate);
+				if (this.percent >= 100) {
 					clearInterval(this.interval);
 					this.interval = null;
 				}
+			}, 30)
+		},
+	});
 
 				if(this.timeout){
 					clearTimeout(this.timeout);
