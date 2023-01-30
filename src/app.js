aa97530351a009b1863a1454a8ad585137748fa2
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-01-30 03:27:38 +0100

    change taost from store to component

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ document.addEventListener('alpine:init', () => {
 		},
 	});
 
-	Alpine.store('toast', {
+	Alpine.data('toast', () => ({
 		visible: false,
 		delay: 5000,
 		percent: 0,
@@ -46,7 +46,7 @@ document.addEventListener('alpine:init', () => {
 				}
 			}, 30)
 		},
-	});
+	}));
 
 	Alpine.data('productItem', () => ({
 		quantity: 1,
@@ -55,15 +55,24 @@ document.addEventListener('alpine:init', () => {
 		},
 		addToCart(quantity = 1) {
 			this.$store.header.cartItems += parseInt(quantity);
-			this.$store.toast.show('The item was added into the cart');
+			this.$dispatch('notify', {
+				message: 'The item was added into the cart'
+			});
+			// this.$store.toast.show('The item was added into the cart');
 		},
 		addToWatchlist(id) {
 			if (this.$store.header.watchingItems.includes(id)) {
 				this.$store.header.watchingItems.splice(this.$store.header.watchingItems.indexOf(id), 1);
-				this.$store.toast.show('The item was removed into the watchlist');
+				this.$dispatch('notify', {
+					message: 'The item was removed into the watchlist'
+				});
+				// this.$store.toast.show('The item was removed into the watchlist');
 			} else {
 				this.$store.header.watchingItems.push(id);
-				this.$store.toast.show('The item was added into the watchlist');
+				this.$dispatch('notify', {
+					message: 'The item was added into the watchlist'
+				});
+				// this.$store.toast.show('The item was added into the watchlist');
 			}
 		},
 		isInWatchlist(id) {
